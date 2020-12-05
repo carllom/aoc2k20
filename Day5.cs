22b7c54729f688b9e9145dad5c883ff6a12a4102
Carl Lom <carl@casalom.se>
2020-12-05 09:15:02 +0100

    Day 5: Binary Boarding

---
+++ Day5.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace aoc2k20
+{
+    class Day5
+    {
+        private static string[] data = File.ReadAllLines("data/5-boardingpass.txt");
+
+        private static int ParseSeating(string seating)
+        {
+            var coord = 0;
+            foreach (var p in seating)
+            {
+                coord <<= 1;
+                if (p == 'B' || p == 'R') coord |= 1;
+            }
+            return coord;
+        }
+
+        public static void Task1()
+        {
+            var q = data.Select(s => ParseSeating(s)).OrderByDescending(s => s).First();
+            Console.WriteLine($"Task #1 - last seat: {q}");
+        }
+        public static void Task2()
+        {
+            var freeSeatAt = 0;
+            var q = data.Select(s => ParseSeating(s)).OrderBy(s => s).ToArray();
+            var lastseat = q[0];
+            for (int i = 1; i < q.Length; i++)
+            {
+                if (q[i] == lastseat + 1) { lastseat = q[i]; } else { freeSeatAt = lastseat + 1; break; }
+            }
+            Console.WriteLine($"Task #2 - free seat: {freeSeatAt}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ namespace aoc2k20
             new Action[] {Day2.Task1, Day2.Task2 },
             new Action[] {Day3.Task1, Day3.Task2 },
             new Action[] {Day4.Task1, Day4.Task2 },
-            new Action[] {DayX.Task1, DayX.Task2 }
+            new Action[] {Day5.Task1, Day5.Task2 },
         };
 
         public void Run(int day, int task)
