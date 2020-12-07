cdc496e34cdc2ad5f1fe4af78ecf87d49e4040c9
Carl Lom <carl.lom@else.se>
2020-12-07 21:22:21 +0100

    Forgot to remove the top bag in task #2..     Rewrote as graph instead of dict.

--- Day7.cs
+++ Day7.cs
@@ -7,64 +7,86 @@ namespace aoc2k20
 {
     class Day7
     {
-        private static string[] data = File.ReadAllLines("data/7-luggage.txt");
-        private static Dictionary<string, List<Tuple<int,string>>> rules = new Dictionary<string, List<Tuple<int,string>>>();
-
-        private static void ParseData()
-        {
-            foreach (var rule in data)
-            {
-                var r = rule.Split(new string[] { " bags contain ", " bags", " bag", ", ", ".", "no other" }, StringSplitOptions.RemoveEmptyEntries);
-                var key = r[0];
-                var rn = new List<Tuple<int,string>>();
-                for (int i = 1; i < r.Length; i++)
-                {
-                    var ws = r[i].IndexOf(' ');
-                    var qty = int.Parse(r[i].Substring(0, ws));
-                    var type = r[i].Substring(ws+1);
-                    rn.Add(new Tuple<int, string>(qty, type));
-                }
-                rules[key] = rn;
-            }
-        }
-
-        private static List<string> AllKeysContaining(string key) => rules.Where(r => r.Value.Any(v => v.Item2 == key)).Select(r => r.Key).ToList();
+        private static readonly string[] Data = File.ReadAllLines("data/7-luggage.txt");
 
         public static void Task1()
         {
-            ParseData();
-            var result = AllKeysContaining("shiny gold");
-            var keys = result;
-            while (true)
-            {
-                var newKeys = new List<string>();
-                foreach (var key in keys)
-                {
-                    newKeys.AddRange(AllKeysContaining(key).Where(k => !result.Contains(k)));
-                }
-                if (newKeys.Count == 0) break;
-                keys = newKeys.Distinct().ToList();
-                result.AddRange(keys);
-            }
-            Console.WriteLine($"Task #1 result: {result.Count()}");
+            var bags = ParseGraph();
+            var count = Ancestors(bags["shiny gold"]).Distinct().Count();
+            Console.WriteLine($"Task #1 result: {count}");
         }
 
-        public static int NumBags(string key)
+        private static ICollection<string> Ancestors(Node bag)
         {
-            if (!rules.ContainsKey(key)) return 1;
-            var count = 1;
-            foreach (var item in rules[key])
+            var parents = new List<string>();
+            foreach (var parent in bag.Parents)
             {
-                count += item.Item1 * NumBags(item.Item2);
+                parents.Add(parent.Name);
+                parents.AddRange(Ancestors(parent));
             }
-            return count;
+            return parents;
         }
 
         public static void Task2()
         {
-            ParseData();
-            var result = NumBags("shiny gold");
+            var bags = ParseGraph();
+            var result = NumBags(bags["shiny gold"]) - 1; // Remove the gold bag from the count.. d-oh!
             Console.WriteLine($"Task #2 result: {result}");
+        }
+
+        private static int NumBags(Node bag)
+        {
+            var count = 1; // This bag
+            foreach (var child in bag.Children)
+            {
+                count += child.Quantity * NumBags(child.Bag);
+            }
+            return count;
+        }
+
+        private class Node
+        {
+            public string Name { get; }
+            public ICollection<Ref> Children { get; } = new List<Ref>();
+            public ICollection<Node> Parents { get; } = new List<Node>();
+            public Node(string name)
+            {
+                Name = name;
+            }
+        }
+
+        private class Ref
+        {
+            public int Quantity { get; }
+            public Node Bag { get; }
+
+            public Ref(Node bag, int quantity)
+            {
+                Bag = bag;
+                Quantity = quantity;
+            }
+        }
+
+        private static Dictionary<string,Node> ParseGraph()
+        {
+            var bags = new Dictionary<string, Node>();
+            foreach (var rule in Data)
+            {
+                var r = rule.Split(new[] { " bags contain ", " bags", " bag", ", ", ".", "no other" }, StringSplitOptions.RemoveEmptyEntries);
+                if (!bags.ContainsKey(r[0])) bags[r[0]] = new Node(r[0]);
+                var bag = bags[r[0]];
+                for (int i = 1; i < r.Length; i++)
+                {
+                    var ws = r[i].IndexOf(' ');
+                    var qty = int.Parse(r[i].Substring(0, ws));
+                    var cName = r[i].Substring(ws + 1);
+                    if (!bags.ContainsKey(cName)) bags[cName] = new Node(cName);
+                    var child = bags[cName];
+                    bag.Children.Add(new Ref(child, qty));
+                    child.Parents.Add(bag);
+                }
+            }
+            return bags;
         }
     }
 }
